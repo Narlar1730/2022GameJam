f8332711719b4d548eb448e47d8bf245467bd277
TheNewScrunt <86963152+TheNewScrunt@users.noreply.github.com>
2022-02-27 20:33:36 +1100

    fixed fullscreen button in menu

--- Game/Menu.gd
+++ Game/Menu.gd
@@ -6,12 +6,26 @@ extends Node2D
 # var b = "text"
 
 var inside = false
+var fullScreenTimer = 0
+
+func updateClocks():
+
+	#FullScreenClock
+	if fullScreenTimer > 0:
+		fullScreenTimer -= 1
 
 func _process(delta):
 	if Input.is_action_just_released("MouseClick") and inside:
 		get_tree().change_scene("res://World.tscn")
+		
+	if Input.is_action_pressed("fullscreen") and !OS.window_fullscreen and fullScreenTimer == 0:
+		OS.window_fullscreen = true
+		fullScreenTimer = 40
+	elif fullScreenTimer == 0 and Input.is_action_pressed("fullscreen"):
+		OS.window_fullscreen = false
+		fullScreenTimer = 40
 
-	
+	updateClocks()
 # Called when the node enters the scene tree for the first time.
 
 func _ready():
