5288418b80f8ea456d9f9e0ced5e53cbbd476e0b
Erik Tults <e.tults@bigpond.com>
2022-02-27 20:20:23 +1100

    Made mainscreen button work

--- Game/Menu.gd
+++ Game/Menu.gd
@@ -5,8 +5,14 @@ extends Node2D
 # var a = 2
 # var b = "text"
 
+var inside = false
 
+func _process(delta):
+	if Input.is_action_just_released("MouseClick") and inside:
+		print("Clicked right!")		
+	
 # Called when the node enters the scene tree for the first time.
+
 func _ready():
 	pass # Replace with function body.
 
@@ -17,10 +23,12 @@ func _ready():
 
 
 func _on_Area2D_mouse_entered():
-	print("entered")
-	pass # Replace with function body.
+	$Area2D/Button.modulate = Color("DDDDDD")
+	inside = true
+
 
 
 func _on_Area2D_mouse_exited():
-	print("exited")
-	pass # Replace with function body.
+	$Area2D/Button.modulate = Color("FFFFFF")
+	inside = false
+
