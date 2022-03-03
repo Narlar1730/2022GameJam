d8f28a01ca342c677ae40043ba0e8f2be05fc0a7
Erik Tults <e.tults@bigpond.com>
2022-03-03 14:43:59 +1100

    Got bat flying

--- Game/Enemies/bossesW1/QueenBat.gd
+++ Game/Enemies/bossesW1/QueenBat.gd
@@ -6,9 +6,15 @@ extends KinematicBody2D
 # var b = "text"
 
 func _process(delta):
-	$AnimationPlayer.animation_set_next("Flight")
+	pass
+
+func flight_animation_finished():
+	$AnimationPlayer.play("Flight")
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	#$AnimationPlayer.get_animation("Flight")
+	$AnimationPlayer.play("Flight")
 	pass # Replace with function body.
 
 
@@ -19,5 +25,6 @@ func _ready():
 
 
 func _on_Area2D_area_entered(area):
-	print("entered!" + str(area))
+	if area.is_in_group("Weapon"):
+		print("entered!")
 	pass # Replace with function body.
