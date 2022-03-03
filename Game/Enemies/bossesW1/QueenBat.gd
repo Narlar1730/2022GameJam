3a36bf8d6b17aade46904c9113358c8de69ad3d3
Erik Tults <e.tults@bigpond.com>
2022-03-03 14:34:23 +1100

    Worked on queen bat hitbox and programming

---
+++ Game/Enemies/bossesW1/QueenBat.gd
@@ -0,0 +1,23 @@
+extends KinematicBody2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+func _process(delta):
+	$AnimationPlayer.animation_set_next("Flight")
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+
+func _on_Area2D_area_entered(area):
+	print("entered!" + str(area))
+	pass # Replace with function body.
